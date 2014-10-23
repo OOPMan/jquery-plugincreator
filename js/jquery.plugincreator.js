240fbca640faf53cfc499b2c48f1b4353598093a
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2014-10-23 18:25:03 +0200

    Re-wrote much of jQuery PluginCreator so as to handle inheritance in a more correct fashion and allow for simple usage of prototypes to access overriden members.

--- js/jquery.plugincreator.js
+++ js/jquery.plugincreator.js
@@ -17,52 +17,15 @@
 "use strict";
 (function () {
     /**
-     * Where a member exists in both hostObject and members, attaches a super function to
-     * the member within members enabling it to call the associated member within hostObject.
-     *
-     * For example:
-     *
-     * hostObject.a = function (text) {
-     *  alert(text);
-     * }
-     *
-     * members.a = function (text) {
-     *  alert(text);
-     *  this.a.super(this, text);
-     * };
-     *
-     * attachSuperFunctions(hostObject, members);
-     *
-     * @param {Object|function} hostObject
-     * @param {Object} members
-     * @returns {*}
-     */
-    function attachSuperFunctions(hostObject, members) {
-        $.each(members, function (memberName, member) {
-            if ($.isFunction(hostObject[memberName])) {
-                var superMember = hostObject[memberName];
-                member.super = function(thiz) {
-                    return superMember.apply(thiz, $.makeArray(arguments).slice(1));
-                };
-            }
-        });
-        return members;
-    }
-    /**
      *
      * @param {jQuery} jQuery
-     * @returns {{addPlugin: addPlugin, extendPlugin: extendPlugin}}
+     * @returns {{addPlugin: addPlugin}}
      */
     function pluginCreatorFactory(jQuery) {
         var $ = jQuery,
             /**
              *
-             * @type {Object}
-             */
-            plugins = {},
-            /**
-             *
-             * @type {{addPlugin: addPlugin, clonePlugin: clonePlugin, extendPlugin: extendPlugin}}
+             * @type {{addPlugin: addPlugin}}
              */
             pluginCreator = {
                 /**
@@ -84,7 +47,9 @@
                 addPlugin: function (name, constructor, defaults, members, prototype) {
                     var constructor = constructor || function () {},
                         defaults = $.extend({}, defaults || {}),
-                        members = [$.extend({}, members || {})],
+                        members = $.extend({}, members || {}),
+                        prototype = prototype || function () {},
+                        prototypes = [prototype],
                         readonlyMembers = {
                             /**
                              *
@@ -105,26 +70,24 @@
                                 return true;
                             }
                         };
+                    $.extend(true, prototype, members, readonlyMembers);
 
                     /**
                      * This function is used to instantiate an instance of the plugin on a given element.
                      *
                      * @param {Object} element
-                     * @param {Object} options
-                     * @param {Array} constructorArguments
+                     * @param {Object} [options]
+                     * @param {Array} [constructorArguments]
                      */
                     function instantiatePlugin(element, options, constructorArguments) {
-                        /**
-                         *
-                         */
-                        var innerConstructor = function () {
-                            this.element = element;
-                            this.context = $(element).addClass(name);
-                            this.options = $.extend(true, {}, $.fn[name].defaults, options);
-                            constructor.apply(this, constructorArguments);
-                        };
-                        if (prototype) innerConstructor.prototype = prototype();
-                        $.extend(true, innerConstructor.prototype, members[0], readonlyMembers);
+                        var options = options || {},
+                            innerConstructor = function () {
+                                this.element = element;
+                                this.context = $(element).addClass(name);
+                                this.options = $.extend(true, {}, $.fn[name].defaults, options);
+                                constructor.apply(this, constructorArguments);
+                            };
+                        innerConstructor.prototype = new prototypes[0]();
                         $.data(element, name, new innerConstructor());
                     }
 
@@ -159,74 +122,55 @@
                      * @param {Object} options
                      * @returns {Object}
                      */
-                    $.fn[name].update = function(options) {
+                    $.fn[name].updateDefaultsWith = function(options) {
                         $.extend(true, $.fn[name].defaults, options);
                         return $.fn[name].defaults;
                     };
                     /**
                      *
-                     * @param {Object} newMembers
-                     * @returns {boolean}
+                     * @param {Object} members
+                     * @param {function} [prototype]
+                     * @returns {function}
                      */
-                    $.fn[name].extend = function(newMembers) {
-                        members.unshift($.extend(true, {}, members[0], attachSuperFunctions(members[0], newMembers)));
-                        return true;
+                    $.fn[name].extendMembersWith = function(members, prototype) {
+                        var prototype = prototype || function prototype() {};
+                        prototype.prototype = new prototypes[0]();
+                        $.extend(true, prototype, members, readonlyMembers);
+                        prototypes.unshift(prototype);
+                        return prototype;
                     };
-                    plugins[name] = {
-                        constructor: constructor,
-                        defaults: $.fn[name].defaults,
-                        members: members,
-                        prototype: prototype
+                    /**
+                     * Clones a plugin added using jQuery PluginCreator.
+                     *
+                     * While the defaults and members are cloned completely and are independent, the constructor is shared.
+                     *
+                     * @param {string} name
+                     * @param {string} newName
+                     * @returns {string}
+                     */
+                    $.fn[name].cloneTo = function (newName) {
+                        return pluginCreator.addPlugin(newName, constructor, defaults, members, prototype);
                     };
+                    /**
+                     * Extends a plugin added using jQuery PluginCreator.
+                     *
+                     * Defaults and members are cloned and extended from the original plugin while the extended constructor
+                     * functions such that the constructor for the original plugin is called first, then new new
+                     * constructor, if present.
+                     *
+                     * @param {string} name
+                     * @param {string} newName
+                     * @param {function()} [constructor]
+                     * @param {Object} [defaults]
+                     * @param {Object} [members]
+                     * @return {string}
+                     */
+                     $.fn[name].extendTo = function (newName, members, prototype) {
+                        var prototype = prototype || function () {};
+                        $.fn[name].cloneTo(newName);
+                        $.fn[newName].extendMembersWith(members, prototype);
+                     };
                     return name;
-                },
-                /**
-                 * Clones a plugin added using jQuery PluginCreator.
-                 *
-                 * While the defaults and members are cloned completely and are independent, the constructor is shared.
-                 *
-                 * @param {string} name
-                 * @param {string} newName
-                 * @returns {string}
-                 */
-                clonePlugin: function(name, newName) {
-                    return pluginCreator.addPlugin(
-                        newName,
-                        plugins[name].constructor,
-                        $.extend(true, {}, plugins[name].defaults),
-                        $.extend(true, {}, plugins[name].members[0]),
-                        plugins[name].prototype
-                    );
-
-                },
-                /**
-                 * Extends a plugin added using jQuery PluginCreator.
-                 *
-                 * Defaults and members are cloned and extended from the original plugin while the extended constructor
-                 * functions such that the constructor for the original plugin is called first, then new new
-                 * constructor, if present.
-                 *
-                 * @param {string} name
-                 * @param {string} newName
-                 * @param {function()} [constructor]
-                 * @param {Object} [defaults]
-                 * @param {Object} [members]
-                 * @return {string}
-                 */
-                extendPlugin: function(name, newName, constructor, defaults, members) {
-                    var constructor = constructor || function () {},
-                        defaults = $.extend(true, {}, defaults || {}),
-                        members = $.extend(true, {}, members || {});
-                    return pluginCreator.addPlugin(
-                        newName,
-                        function() {
-                            plugins[name].constructor.apply(this, arguments);
-                            constructor.apply(this, arguments);
-                        },
-                        $.extend(true, {}, plugins[name].defaults, defaults),
-                        $.extend(true, {}, plugins[name].members[0], attachSuperFunctions(plugins[name].members[0], members)),
-                        plugins[name].prototype
-                    );
                 }
         };
         return pluginCreator;
