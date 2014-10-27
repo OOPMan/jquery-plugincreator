4382121adbd3a6227fbf26c8e767d66465663783
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2014-10-27 19:54:22 +0200

    Fixed creation of initial prototype during addPlugin. Amended extend function defined as part of readonlyMembers.

--- test/addPlugin.js
+++ test/addPlugin.js
@@ -31,9 +31,11 @@ describe("require('pluginCreator')", function () {
             });
         });
         describe("jQuery('#unique').test2()", function () {
-            it("should instantiate test2 on #unique, calling constructorFunction in the process", function () {
+            it("should instantiate test2 on #unique", function () {
                 test.number(jQuery("#unique").length).is(1);
                 jQuery("#unique").test2();
+            });
+            it("should have called `constructorFunction` during instantiation", function () {
                 test.object(jQuery("#unique").data("jquery-plugincreator-test2"))
                         .hasProperty("test2", true);
             });
@@ -41,16 +43,48 @@ describe("require('pluginCreator')", function () {
         describe("pluginCreator.addPlugin('test3', null, defaults)", function () {
             it("should create jQuery.fn.test3", function () {
                 pluginCreator.addPlugin("test3", null, {test3: true});
+                test.function(jQuery.fn.test3);
             });
         });
         describe("jQuery('#unique').test3()", function () {
-            it("should instantiate test3 on #unique, setting copy defaults to the options member in the process", function () {
+            it("should instantiate test3 on #unique", function () {
                 test.number(jQuery("#unique").length).is(1);
                 jQuery("#unique").test3();
-                test.object(jQuery("#unique").data("jquery-plugincreator-test3"))
+            });
+            it("should make `defaults` available to the instance via the `options` member created during instantiation using the `defaults` supplied to pluginCreator.addPlugin('test3', null, defaults)", function () {
+                var instance = jQuery("#unique").data("jquery-plugincreator-test3");
+                test.object(instance)
                         .hasProperty("options")
-                    .object(jQuery("#unique").data("jquery-plugincreator-test3").options)
+                    .object(instance.options)
                         .hasProperty("test3", true);
+            });
+        });
+        var test4FunctionResult = null;
+        describe("pluginCreator.addPlugin('test4', null, null, members)", function () {
+            it("should create jQuery.fn.test4", function () {
+                pluginCreator.addPlugin("test4", null, null, {
+                    test4Value: true,
+                    test4Function1: function () {
+                        test4FunctionResult = true;
+                    },
+                    test4Function2: function(parameter) {
+                        test4FunctionResult = parameter;
+                    }
+                });
+                test.function(jQuery.fn.test4);
+            });
+        });
+        describe("jQuery('#unique').test4()", function () {
+            it("should instantiate test4 on #unique", function () {
+                test.number(jQuery("#unique").length).is(1);
+                jQuery("#unique").test4();
+            });
+            it("should make `members` available to the instance via the prototype created by calling pluginCreator.addPlugin('test4', null, null, members)", function() {
+                var instance = jQuery("#unique").data("jquery-plugincreator-test4");
+                test.object(instance)
+                        .hasProperty("test4Value", true)
+                    .function(instance.test4Function1)
+                    .function(instance.test4Function2);
             });
         });
         after(function () {
